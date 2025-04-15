4ae9e5d70beefeed6aa6e404a61f42300f49a1c7
Jovianto <joviantogodjali@gmail.com>
2025-04-15 14:35:40 +0700

    change development onto PHP

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Jovianto Godjali</title>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/styles.css" rel="stylesheet">
+</head>
+<body>
+  <?php include 'include/navbar.php'; ?>
+
+  <div class="main-home-container container d-flex flex-column">
+    <h1 class="bold text-center">Jovianto Godjali</h1>
+    <p style="font-size: 36px;">also known as Akshiro</p>
+  </div>
+  
+  <script src="js/bootstrap.bundle.min.js"></script>
+  <script src="js/gsap.min.js"></script>
+  <script src="js/scripts.js"></script>
+</body>
+</html>
